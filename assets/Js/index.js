fd9caf8af3cbf5e7b1d7401b5b9f5010c21a16ac
Vinicius Bueno <BuenoVini455@hotmail.com>
2024-05-13 09:57:53 -0300

    up

--- assets/Js/index.js
+++ assets/Js/index.js
@@ -1,15 +1,18 @@
-window.addEventListener("keydown", function (e) {
+function playSound(e) {
 	const audio = document.querySelector(`audio[data-kay="${e.keycode}"]`);
 	const key = document.querySelector(`.key[data-key="${e.kaycode}"]`);
 	if (!audio) return;
 	audio.currentTime = 0;
 	audio.play();
 	key.classList.add("playing");
-});
+	setTimeout(function () {}, 0.07);
+}
 
 function removeTransition(e) {
 	if (e.propertyName !== "transition") return;
+	this.classList.remove("playing");
 }
 
 const keys = document.querySelectorAll(".key");
 keys.forEach((key) => key.addEventListener("transitionend", removeTransition));
+window.addEventListener("keydown", playSound);
